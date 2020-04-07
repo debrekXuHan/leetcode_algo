c488457d5bdde4df986e6167b2801d8bdc7d9228
debrekXuHan <debrek_xuhan@hotmail.com>
2020-04-07 22:34:29 +0800

    upload Question 17 C codes

--- c/17.letter-combinations-of-a-phone-number.c
+++ c/17.letter-combinations-of-a-phone-number.c
@@ -42,9 +42,45 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char ** letterCombinations(char * digits, int* returnSize){
+    char *array[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    int  choices[10] = {0, 0, 3, 3, 3, 3, 3, 4, 3, 4};
 
+    char *pDigits = digits;
+    int len = 0, size = 1;
+    while (*pDigits) {
+        if (0 == choices[*pDigits - '0']) {  // inpput array with "0" or "1"
+            len = 0; break;
+        }
+        size *= choices[*pDigits - '0'];
+        pDigits++; len++;
+    }
+
+    // empty input array or array with "0" or "1"
+    if (!len) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int res_size = size, cur_size = size;
+    *returnSize = res_size;
+    char **pRet = (char **)malloc(sizeof(char *) * res_size);
+    for (int i = 0; i < size; i++) {
+        pRet[i] = (char *)malloc(sizeof(char) * (len + 1));
+    }
+
+    pDigits = digits;
+    int index = 0;
+    while (*pDigits) {
+        cur_size /= choices[*pDigits - '0'];
+        char *pArray = array[*pDigits - '0'];
+        for (int i = 0; i < size; i++) {
+            pRet[i][index] = pArray[(i / cur_size) % choices[*pDigits - '0']];
+        }
+        pDigits++; index++;
+    }
+    for (int i = 0; i < size; i++) {
+        pRet[i][index] = '\0';
+    }
+    return pRet;
 }
-
-
 // @lc code=end
-
